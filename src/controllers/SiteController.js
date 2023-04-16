0e707365bd5ef3cbd25d6b08227b9917ed37935a
TomTom2k <tramthu96@gmail.com>
2023-04-17 04:28:04 +0700

    add home page and info page

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -2,6 +2,11 @@ const renderHomePage = (req, res, next) => {
 	res.render('pages/home');
 };
 
+const renderInfoPage = (req, res, next) => {
+	res.render('pages/info');
+};
+
 module.exports = {
 	renderHomePage,
+	renderInfoPage,
 };
