ff90b1348d83626da42a21cdf747eceacb654376
SiZedYi <anhthang764@gmail.com>
2023-09-10 12:44:46 +0700

    add Docker file

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -5,13 +5,13 @@ var fs = require("fs");
 require('dotenv').config();
 var myCon = mysql.createConnection({
   host: process.env.HOST_NAME || "localhost",
-  port: process.env.PORT || "3306",
+  port: process.env.DB_PORT || "3306",
   //  database: process.env.DB_NAME || 'h32_blog',
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "anhthang123",
 });
 
-fs.readFile("h32blog.sql", "utf8", function (err, data) {
+fs.readFile("dump_file.sql", "utf8", function (err, data) {
   if (err) {
     console.log(err);
     return;
